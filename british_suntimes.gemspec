37cd2c92b3d80d88faf73e8cfa5ec9206e77235b
Your Name <you@example.com>
2018-03-08 23:36:05 +0000

    1st commit

---
+++ british_suntimes.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |s|
+  s.name = 'british_suntimes'
+  s.version = '0.1.0'
+  s.summary = 'Generates the British sunrise and sunset times.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/british_suntimes.rb']
+  s.add_runtime_dependency('dynarex', '~> 1.7', '>=1.7.29')
+  s.add_runtime_dependency('geocoder', '~> 1.4', '>=1.4.6')
+  s.add_runtime_dependency('chronic_cron', '~> 0.3', '>=0.3.7')
+  s.add_runtime_dependency('RubySunrise', '~> 0.3', '>=0.3.1')  
+  s.signing_key = '../privatekeys/british_suntimes.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/british_suntimes'
+end
---
+++ lib/british_suntimes.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+# file: british_suntimes.rb
+
+
+require 'dynarex'
+require 'geocoder'
+require 'chronic_cron'
+require 'solareventcalculator'
+
+
+
+class BritishSuntimes
+
+  attr_reader :to_h, :bst_start, :bst_end
+
+
+  def initialize(year= Date.today.year.to_s, location: 'edinburgh')
+
+    a = (Date.parse(year + ' Jan')...Date.parse(year.succ + ' Jan')).to_a
+    g = Geocoder.search(location)
+
+    times = a.inject({}) do |r, date|
+
+      calc = SolarEventCalculator.new(date, *g[0].coordinates)
+
+      a2 = %w(sunrise sunset).map do |event|
+        calc.method('compute_utc_official_'.+(event).to_sym).call\
+          .strftime("%H:%M")
+      end
+
+      r.merge(date.strftime("%d %b") => a2)
+    end
+
+    # alter the times for British summertime
+
+    d1 = ChronicCron.new('last sunday in March at 1am', 
+          Time.local(year)).to_date
+    d2 = ChronicCron.new('last sunday in October at 2am', 
+          Time.local(year)).to_date
+
+    @bst_start, @bst_end = d1, d2
+
+    (d1...d2).to_a.each do |d|
+      t1, t2 = times[d.strftime("%d %b")].map {|x| Time.parse(x)}
+      t1 += 60 * 60; t2 += 60 * 60
+      times[d.strftime("%d %b")] = [t1.strftime("%H:%M"), t2.strftime("%H:%M")]
+    end
+
+    @to_h = times  
+
+  end
+
+end
+
+
+if __FILE__ == $0 then
+
+  h = BritishSuntimes.new.to_h
+
+end
