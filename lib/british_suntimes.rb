8104932e398492d06cff0035acba4d6fc33c254b
Your Name <you@example.com>
2018-03-09 10:02:15 +0000

    feature: Times can now be exported to a raw Dynarex file.

--- lib/british_suntimes.rb
+++ lib/british_suntimes.rb
@@ -3,6 +3,7 @@
 # file: british_suntimes.rb
 
 
+require 'dynarex'
 require 'geocoder'
 require 'chronic_cron'
 require 'solareventcalculator'
@@ -11,10 +12,10 @@ require 'solareventcalculator'
 
 class BritishSuntimes
 
-  attr_reader :to_h, :bst_start, :bst_end
+  attr_reader :to_h, :bst_start, :bst_end, :to_dx
 
 
-  def initialize(year= Date.today.year.to_s, location: 'edinburgh')
+  def initialize(year=Date.today.year.to_s, location: 'edinburgh')
 
     a = (Date.parse(year + ' Jan')...Date.parse(year.succ + ' Jan')).to_a
     g = Geocoder.search(location)
@@ -33,22 +34,45 @@ class BritishSuntimes
 
     # alter the times for British summertime
 
-    d1 = ChronicCron.new('last sunday in March at 1am', 
+    @bst_start = ChronicCron.new('last sunday in March at 1am', 
           Time.local(year)).to_date
-    d2 = ChronicCron.new('last sunday in October at 2am', 
+    @bst_end = ChronicCron.new('last sunday in October at 2am', 
           Time.local(year)).to_date
 
-    @bst_start, @bst_end = d1, d2
 
-    (d1...d2).to_a.each do |d|
+    (@bst_start...@bst_end).to_a.each do |d|
       t1, t2 = times[d.strftime("%d %b")].map {|x| Time.parse(x)}
       t1 += 60 * 60; t2 += 60 * 60
       times[d.strftime("%d %b")] = [t1.strftime("%H:%M"), t2.strftime("%H:%M")]
     end
 
-    @to_h = times  
-
+    @year, @location, @to_h = year, location, times
+    @to_dx = build_dx()
+    
   end
+  
+  # exports the sun times to a raw Dynarex file
+  #
+  def export(filename=@location + "-suntimes-#{@year}.txt")
+    File.write filename, @to_dx.to_s
+  end  
+  
+  private
+  
+  def build_dx()
+
+    dx = Dynarex.new 'times[title, tags, desc, bst_start, ' + 
+        'bst_end]/day(date, sunrise, sunset)'
+    dx.title = @location + " sunrise sunset times " + @year
+    dx.tags = @location + " times sunrise sunset %s" % [@year]
+    dx.delimiter = ' # '
+    dx.bst_start, dx.bst_end = @bst_start.to_s, @bst_end.to_s
+    dx.desc = 'Adjusted for British summertime'
+
+    @to_h.each {|k,v| dx.create date: k, sunrise: v[0], sunset: v[1] }     
+    
+    dx
+  end  
 
 end
 
